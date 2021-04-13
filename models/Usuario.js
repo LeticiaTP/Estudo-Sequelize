69e181cd66691d67f33c5ef1e52c54afcf210fd2
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-04-13 10:20:17 -0300

    relação n:m

--- models/Usuario.js
+++ models/Usuario.js
@@ -13,6 +13,13 @@ module.exports = (sequelize, DataTypes) => {
 
     Usuario.associate = (models) => {
         Usuario.hasMany(models.Post, {as:"posts", foreignKey:"usuarios_id"});
+        Usuario.belongsToMany(models.Post, {
+            as: "curtiu", // alias da relação
+            through: "curtidas", // tabela intermediária
+            foreignKey: "usuarios_id",
+            otherKey: "posts_id",
+            timestamps: false
+        })
     }
 
     return Usuario;
